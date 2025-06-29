0f1f01fa83347dc73baa0c640aa4872840492bca
patriajasel <105503699+patriajasel@users.noreply.github.com>
2025-06-29 19:12:27 +0800

    Splash Screen Updates
    - Added Splash Screen Page
    - Added App Colors
    - Added App Them Config
    - Added 3 themes of the app
    - Added a custom title bar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:church_songbook_app/features/splash_screen/splash_screen.dart';
+import 'package:church_songbook_app/shared/theme/app_theme_config.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:window_manager/window_manager.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const Main());
+  await windowManager.ensureInitialized();
+
+  await windowManager.center(animate: true);
+  await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
+  await windowManager.setIcon('lib/shared/assets/sda_logo.png');
+
+  runApp(
+    ScreenUtilInit(
+      designSize: Size(1440, 900),
+      builder: (_, child) => const Main(),
+    ),
+  );
 }
 
 class Main extends StatelessWidget {
@@ -11,6 +25,10 @@ class Main extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplashScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeConfig.redTealTheme,
+      home: SplashScreen(),
+    );
   }
 }
