c9205e8d85254b55b7c73e6e6e941c9bdacb10b7
Jasel Patria <patriajasel@gmail.com>
2026-02-14 22:27:22 +0800

    enhancements: windows manager configuration enhancements
    - added setting window title and icon
    - added setting window fullscreen on and off
    - resetted app icon
    - app version bumped

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:church_songbook_app/config/app.config.dart';
 import 'package:church_songbook_app/modules/_master/master_page.dart';
 import 'package:church_songbook_app/util/window_manager/window_manager.util.dart';
 import 'package:flutter/material.dart';
@@ -5,6 +6,10 @@ import 'package:flutter/material.dart';
 Future<void> mainCommon() async {
   WidgetsFlutterBinding.ensureInitialized();
   await WindowManagerUtil.init();
+  await WindowManagerUtil.setTitle(
+    title: "${AppConfig().appName} version ${AppConfig().appVersion}",
+  );
+  await WindowManagerUtil.setIcon(iconPath: 'assets/logo/sda_logo.png');
 
   runApp(const MasterPage());
 }
--- lib/util/window_manager/window_manager.util.dart
+++ lib/util/window_manager/window_manager.util.dart
@@ -1,18 +1,26 @@
+import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
 
 class WindowManagerUtil {
   static Future<void> init() async {
     await windowManager.ensureInitialized();
     await windowManager.center(animate: true);
-    await windowManager.setTitleBarStyle(TitleBarStyle.hidden);
-    await windowManager.setIcon('lib/shared/assets/sda_logo.png');
+    await windowManager.setBackgroundColor(Colors.white);
   }
 
-  static Future<void> setWindowIcon(String path) async {
-    await windowManager.setIcon(path);
-  }
-
-  static Future<void> setWindowTitle(String title) async {
+  static Future<void> setTitle({required String title}) async {
     await windowManager.setTitle(title);
+  }
+
+  static Future<void> setIcon({required String iconPath}) async {
+    await windowManager.setIcon(iconPath);
+  }
+
+  static Future<void> setFullScreenOn() {
+    return windowManager.setFullScreen(true);
+  }
+
+  static Future<void> setFullScreenOff() {
+    return windowManager.setFullScreen(false);
   }
 }
